4b79f3581e8f78b2bac41bb1f1186db4f08c43eb
Jason Rastovski <jasonr@asebp.ca>
2020-03-15 13:36:45 -0600

    Start of demo 2

--- src/basic-component.tsx
+++ src/basic-component.tsx
@@ -1,5 +1,39 @@
 import React from "react";
 
+import { Form, Field, FormRenderProps } from "react-final-form";
+
 export const BasicComponent: React.FunctionComponent = () => {
-  return <div>basic text</div>;
+  const required = (value: string) => (value ? undefined : "Required");
+
+  return (
+    <Form
+      onSubmit={formObject => console.log(formObject)}
+      initialValues={{ firstName: "John", lastName: "Smith" }}
+    >
+      {(props: FormRenderProps) => (
+        <form onSubmit={props.handleSubmit}>
+          <Field
+            name="firstName"
+            component="input"
+            placeholder="First Name"
+            validate={required}
+          />
+          <Field
+            name="lastName"
+            component="input"
+            placeholder="Last Name"
+            validate={required}
+          />
+          <br />
+          <button
+            type="submit"
+            disabled={props.pristine || props.hasValidationErrors}
+          >
+            Submit
+          </button>
+          <pre>{JSON.stringify(props, null, "\t")}</pre>
+        </form>
+      )}
+    </Form>
+  );
 };
