3ad4ecf1d6a9eab4b96064c714e2ed7475babcbe
Jason Rastovski <jasonr@asebp.ca>
2020-03-15 12:25:56 -0600

    Adds a basic component to maninpulate during demos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { BasicComponent } from "./basic-component";
 
 function App() {
-  return <div className="App">Empty application</div>;
+  return (
+    <div className="App">
+      <BasicComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/basic-component.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const BasicComponent: React.FunctionComponent = () => {
+  return <div>basic text</div>;
+};
